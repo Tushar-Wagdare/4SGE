26b64282a4a97006fea326a8566c5d0da8c10605
Tushar Wagdare <tushartwagdare@gmail.com>
2024-07-26 13:46:06 +0530

    Refactore code structure : code structure refactored

--- 4SGE_CODE/src/core/OGL.cpp
+++ 4SGE_CODE/src/core/OGL.cpp
@@ -8,46 +8,19 @@
 #include<C:/glew-2.1.0/include/GL/glew.h>
 #include<gl/GL.h>
 #include"..\..\include\math\vmath.h"
-
+//*** Name space ***
 using namespace vmath;
-
-//*** MACRO'S ***
-#define WIN_WIDTH 800
-#define WIN_HIGHT 600
 
 //*** link With OpenGL Library//
 #pragma comment(lib, "glew32.lib")
 #pragma comment(lib, "OpenGL32.lib")
 
-//*** Globle Function Declarations ***
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-
-//*** Globle Variable Declaration ***
-FILE* gpFILE           = NULL;
-HWND ghwnd             = NULL;
-BOOL gbActive          = FALSE; 
-DWORD dwStyle          = 0;
-WINDOWPLACEMENT wpPrev = { sizeof(WINDOWPLACEMENT) };
-BOOL gbFullscreen      = FALSE;
-//*** OpenGL Related Globle Variables ***
-PIXELFORMATDESCRIPTOR pfd;
-int iPexelFormatIndex  = 0;
-HDC ghdc               = NULL;
-HGLRC ghrc             = NULL;
-
 ///PP Variables
 GLuint shaderProgramObject  = 0;
-enum
-{
-	AMC_ATTRIBUTE_POSITION  = 0,
-	AMC_ATTRIBUTE_COLOR
-};
 GLuint vao                  = 0;
 GLuint vbo_position         = 0;
-GLuint vbo_color = 0;
-
-GLuint mvpMatrixUniform = 0;
-
+GLuint vbo_color            = 0;
+GLuint mvpMatrixUniform     = 0;
 mat4 perspectiveProjectionMatrix;//in vmath.h
 
 
